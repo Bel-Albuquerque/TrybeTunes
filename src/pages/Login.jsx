580d5d22fd1fe0c9cccfebf9ba262bb7dc6fea7d
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-01-17 15:47:43 -0300

    implementa login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import Button from '../components/Button';
+import InputName from '../components/InputName';
+import LoginContext from '../Contexts/LoginContext';
+import { createUser } from '../services/userAPI';
 
 function Login() {
-  return (
+  const { disabled, name } = useContext(LoginContext);
+  const [loading, setLoading] = useState(false);
+  const history = useHistory();
 
+  const handleClick = async () => {
+    setLoading(true);
+    await createUser({ name });
+    history.push('/search');
+  };
+
+  return (
     <main data-testid="page-login">
-      Login
+      {loading ? 'Carregando...' : (
+        <>
+          <InputName />
+          <Button
+            text="Entrar"
+            disabled={ disabled }
+            handleClick={ handleClick }
+          />
+        </>
+      )}
     </main>
   );
 }
