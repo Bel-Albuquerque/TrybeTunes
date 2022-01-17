35c2127b6329d00191f923802fd9e5aa8e9686bd
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-01-17 20:37:43 -0300

    finaliza implementaçao de busca

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,8 +1,9 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useState } from 'react/cjs/react.development';
 import Button from '../components/Button';
 import Header from '../components/Header';
 import InputSearch from '../components/InputSearch';
+import MapCardAlbum from '../components/MapCardAlbum';
 import SearchContext from '../Contexts/SearchContext/SearchContext';
 import searchAlbumsAPIs from '../services/searchAlbumsAPI';
 
@@ -10,18 +11,29 @@ function Search() {
   const {
     searchArtist,
     setSearchArtist,
-    searchAlbuns,
     setSearchAlbuns,
   } = useContext(SearchContext);
 
   const [loading, setLoading] = useState(false);
+  const [menssage, setMessage] = useState('');
+
+  const searchMenssage = (albuns) => {
+    if (albuns.length < 1) {
+      const msgm = 'Nenhum álbum foi encontrado';
+      setMessage(msgm);
+      return setSearchArtist('');
+    }
+    const msgm = `Resultado de álbuns de: ${searchArtist}`;
+    setMessage(msgm);
+    setSearchArtist('');
+  };
 
   const handleSearchClick = async () => {
     await setLoading(true);
     const albuns = await searchAlbumsAPIs(searchArtist);
     setSearchAlbuns(albuns);
-    setSearchArtist('');
     setLoading(false);
+    searchMenssage(albuns);
   };
 
   return (
@@ -38,6 +50,8 @@ function Search() {
               handleClick={ handleSearchClick }
               dataTestId="search-artist-button"
             />
+            { menssage }
+            <MapCardAlbum />
           </>
         )}
       </section>
