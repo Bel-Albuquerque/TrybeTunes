8190b6fdffe173a026b1bb46f4fe3f6f2a178893
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2022-01-18 17:09:11 -0300

    implementa componentes na page Album

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useState } from 'react/cjs/react.development';
 import Header from '../components/Header';
+import InfoAlbum from '../components/InfoAlbum';
+import MapMusicCard from '../components/MapCardMusic';
+import AlbumContext from '../Contexts/AlbumContext/AlbumContext';
+import getMusics from '../services/musicsAPI';
 
 function Album() {
+  const { data, setData } = useContext(AlbumContext);
+
+  const [loading, setLoading] = useState(true);
+
+  const pathArray = window.location.pathname.split('/');
+  const id = pathArray[2];
+
+  useEffect(() => {
+    (async () => {
+      const songs = await getMusics(id);
+
+      await setData(songs);
+      await setLoading(false);
+    })();
+  }, []);
+
   return (
     <main data-testid="page-album">
       <Header />
-      Album
+      { loading && 'Carregando...' }
+      { !loading && <InfoAlbum arrayData={ data } />}
+      { !loading && <MapMusicCard arrayData={ data } /> }
     </main>
   );
 }
